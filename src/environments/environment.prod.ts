d66be4e6d0923e700ff96962775f63e223ae76f1
Brice150 <brice.lecomte0@gmail.com>
2023-04-02 11:18:05 +0200

    AWS deploy test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:5000',
+  apiBaseUrl: 'http://pickme-env.eba-2eftvtmr.eu-west-3.elasticbeanstalk.com',
   imagePath: './assets/images/'
 };
