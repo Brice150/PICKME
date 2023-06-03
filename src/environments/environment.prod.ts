c056a54f1fc1479271b641d1e9db4393eb138d4e
Brice150 <brice.lecomte0@gmail.com>
2023-06-03 20:32:43 +0200

    new prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://pickme-production.up.railway.app/',
+  apiBaseUrl: 'https://pickme-production.up.railway.app',
   imagePath: './assets/images/'
 };
