ef233b557f6edaefad6125fec1e5b5a5a4e503ce
brice150 <brice.lecomte0@gmail.com>
2024-03-18 15:13:58 +0100

    stats 2

--- backend/src/main/java/com/packages/backend/repository/AdminRepository.java
+++ backend/src/main/java/com/packages/backend/repository/AdminRepository.java
@@ -1,12 +1,28 @@
 package com.packages.backend.repository;
 
 import com.packages.backend.model.user.User;
+import com.packages.backend.model.user.UserStats;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AdminRepository extends JpaRepository<User, Long> {
-  @Query("SELECT u FROM User u ORDER BY u.nickname")
-  List<User> getAllUsers();
+  @Query(
+    "SELECT u FROM User u " +
+      " WHERE u.id != :connectedId" +
+      " ORDER BY u.nickname"
+  )
+  List<User> getAllUsers(@Param("connectedId") Long connectedId);
+
+  @Query(
+    "SELECT u.id, count(d) as totalDislikes, count(l) as totalLikes, 0 as totalMatches" +
+      " FROM User u " +
+      " LEFT JOIN Dislike d ON u.id = d.fkReceiver" +
+      " LEFT JOIN Like l ON u.id = l.fkReceiver" +
+      " WHERE u.id != :connectedId" +
+      " GROUP BY u.id"
+  )
+  List<UserStats> getAllUsersStats(@Param("connectedId") Long connectedId);
 }
--- backend/src/main/java/com/packages/backend/service/AdminService.java
+++ backend/src/main/java/com/packages/backend/service/AdminService.java
@@ -1,12 +1,17 @@
 package com.packages.backend.service;
 
+import com.packages.backend.model.user.User;
 import com.packages.backend.model.user.UserDTO;
 import com.packages.backend.model.user.UserDTOMapper;
+import com.packages.backend.model.user.UserStats;
 import com.packages.backend.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class AdminService {
@@ -22,7 +27,20 @@ public class AdminService {
   }
 
   public List<UserDTO> getAllUsers() {
-    return adminRepository.getAllUsers().stream().map(userDTOMapper).toList();
+    User connectedUser = userService.getConnectedUser();
+    List<User> users = adminRepository.getAllUsers(connectedUser.getId());
+    List<UserStats> usersStats = adminRepository.getAllUsersStats(connectedUser.getId());
+    Map<Long, UserStats> userStatsMap = usersStats.stream()
+      .collect(Collectors.toMap(UserStats::getId, Function.identity()));
+    users.forEach(user -> {
+      UserStats userStats = userStatsMap.get(user.getId());
+      if (userStats != null) {
+        user.setTotalDislikes(userStats.getTotalDislikes());
+        user.setTotalLikes(userStats.getTotalLikes());
+        user.setTotalMatches(userStats.getTotalMatches());
+      }
+    });
+    return users.stream().map(userDTOMapper).toList();
   }
 
   public void deleteUserById(Long userId) {
