a93b4b54fcbb855a2e5547a980c033c7a27248ed
brice150 <brice.lecomte0@gmail.com>
2024-03-22 10:33:46 +0100

    admin search working

---
+++ backend/src/main/java/com/packages/backend/model/AdminSearch.java
@@ -0,0 +1,34 @@
+package com.packages.backend.model;
+
+import com.packages.backend.model.user.UserDTO;
+
+import java.util.List;
+
+public class Match {
+  private UserDTO user;
+  private List<Message> messages;
+
+  public Match() {
+  }
+
+  public Match(UserDTO user, List<Message> messages) {
+    this.user = user;
+    this.messages = messages;
+  }
+
+  public UserDTO getUser() {
+    return user;
+  }
+
+  public void setUser(UserDTO user) {
+    this.user = user;
+  }
+
+  public List<Message> getMessages() {
+    return messages;
+  }
+
+  public void setMessages(List<Message> messages) {
+    this.messages = messages;
+  }
+}
