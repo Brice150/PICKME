0e4c1683c940e4c6abbcbcffd6085571ade41437
brice150 <brice.lecomte0@gmail.com>
2024-04-03 11:59:07 +0200

    cors filter update

--- backend/src/main/java/com/packages/backend/security/config/MyBasicAuthenticationEntryPoint.java
+++ backend/src/main/java/com/packages/backend/security/config/MyBasicAuthenticationEntryPoint.java
@@ -1,13 +1,20 @@
 package com.packages.backend.security.config;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 @Component
 public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
   /**
@@ -17,12 +24,29 @@ public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoi
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
     throws IOException {
     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.setHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
     PrintWriter writer = response.getWriter();
     writer.println("HTTP Status 401 - " + authEx.getMessage());
   }
+
   @Override
   public void afterPropertiesSet() {
     setRealmName("YOUR REALM");
     super.afterPropertiesSet();
+  }
+
+  @Bean
+  @Order(Ordered.HIGHEST_PRECEDENCE)
+  public CorsFilter corsFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("http://localhost:4200");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("*");
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsFilter(source);
   }
 }
