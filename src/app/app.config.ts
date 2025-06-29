f8c5253a3a218b20ab4f1c550796836abfe31ab4
brice150 <brice.lecomte0@gmail.com>
2025-06-29 12:13:50 +0200

    angular update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,18 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, withHashLocation } from '@angular/router';
-import { provideToastr } from 'ngx-toastr';
-import { routes } from './app.routes';
-import { register as registerSwiperElements } from 'swiper/element/bundle';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
+import { provideToastr } from 'ngx-toastr';
+import { register as registerSwiperElements } from 'swiper/element/bundle';
+import { routes } from './app.routes';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
 
 registerSwiperElements();
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation()),
+    provideRouter(routes),
     provideHttpClient(withInterceptors([errorInterceptor])),
     provideToastr(),
     provideAnimations(),
